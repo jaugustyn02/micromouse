10cef6c2cd4976e181232d19a89ac255138aae74
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2025-03-23 20:04:08 +0100

    MM-9 - generate random maze using backtracking

--- src/model/Maze.cpp
+++ src/model/Maze.cpp
@@ -10,12 +10,52 @@ Maze::Maze(int width, int height) : width(width), height(height) {
 void Maze::generate() {
   resetGrid();
 
-  for (int i = 0; i < width; ++i) {
-    for (int j = 0; j < height; ++j) {
-      grid[j][i].topWall = Randomizer::GetRandomBool();
-      grid[j][i].rightWall = Randomizer::GetRandomBool();
-      grid[j][i].bottomWall = Randomizer::GetRandomBool();
-      grid[j][i].leftWall = Randomizer::GetRandomBool();
+  std::stack<Cell *> stack;
+  Cell *currentCell = &grid[0][0];
+  currentCell->visited = true;
+  stack.push(currentCell);
+
+  while (!stack.empty()) {
+    std::vector<Cell *> neighbors;
+    int x = currentCell->x;
+    int y = currentCell->y;
+
+    if (x > 0 && !grid[y][x - 1].visited) {
+      neighbors.push_back(&grid[y][x - 1]);
+    }
+    if (y > 0 && !grid[y - 1][x].visited) {
+      neighbors.push_back(&grid[y - 1][x]);
+    }
+    if (x < width - 1 && !grid[y][x + 1].visited) {
+      neighbors.push_back(&grid[y][x + 1]);
+    }
+    if (y < height - 1 && !grid[y + 1][x].visited) {
+      neighbors.push_back(&grid[y + 1][x]);
+    }
+
+    if (!neighbors.empty()) {
+      Cell *nextCell = neighbors[Randomizer::GetRandom(0, (int) neighbors.size() - 1)];
+      stack.push(nextCell);
+
+      if (nextCell->x < currentCell->x) {
+        currentCell->leftWall = false;
+        nextCell->rightWall = false;
+      } else if (nextCell->x > currentCell->x) {
+        currentCell->rightWall = false;
+        nextCell->leftWall = false;
+      } else if (nextCell->y < currentCell->y) {
+        currentCell->topWall = false;
+        nextCell->bottomWall = false;
+      } else if (nextCell->y > currentCell->y) {
+        currentCell->bottomWall = false;
+        nextCell->topWall = false;
+      }
+
+      currentCell = nextCell;
+      currentCell->visited = true;
+    } else {
+      currentCell = stack.top();
+      stack.pop();
     }
   }
 }
